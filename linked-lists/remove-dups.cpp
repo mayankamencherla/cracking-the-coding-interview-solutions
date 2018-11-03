c1ddd3ea6a96288cf56feab31f06523917d8b44c
Mayank Amencherla <mayankamencherla24@gmail.com>
2018-11-03 19:28:30 +0000

    Create remove-dups.cpp

---
+++ linked-lists/remove-dups.cpp
@@ -0,0 +1,77 @@
+#include <list>
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+#include "linked-list.cpp"
+
+using namespace std;
+
+void printList(LinkedList& l)
+{
+    Node* curr = l.getHead();
+
+    while (curr)
+    {
+        cout << curr->val << " ";
+
+        curr = curr->next;
+    }
+
+    cout << endl;
+}
+
+/**
+ * This method takes in a linked list and removes duplicates using a set
+ * The set contains all the elements that we have seen so far
+ * This method creates a new linked list after deleting elements
+ */
+LinkedList removeDupsWithBuffer(LinkedList& l)
+{
+    unordered_set<int> hash;
+
+    LinkedList l2 = LinkedList();
+
+    Node* curr = l.getHead();
+
+    while (curr)
+    {
+        Node* next = curr->next;
+
+        if (hash.count(curr->val) == 0)
+        {
+            curr->next = NULL;
+
+            l2.addNode(curr);
+        }
+
+        hash.insert(curr->val);
+
+        curr = next;
+    }
+
+    return l2;
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 1, 2, 1, 2, 4, 5, 2, 4, 5, 2, 4};
+
+    LinkedList l = LinkedList();
+
+    for (int elem : v)
+    {
+        l.addNode(elem);
+    }
+
+    cout << "Printing list before removing dups" << endl;
+
+    printList(l);
+
+    cout << endl;
+
+    l = removeDupsWithBuffer(l);
+
+    cout << "Printing list after removing dups using buffer" << endl;
+
+    printList(l);
+}
