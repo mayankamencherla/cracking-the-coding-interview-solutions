5ca1655de8eff62821de766c5192f0539791a453
Mayank Amencherla <mayankamencherla24@gmail.com>
2018-11-05 16:30:05 +0000

    Update animal.cpp

--- stacks-and-queues/animal.cpp
+++ stacks-and-queues/animal.cpp
@@ -10,10 +10,13 @@ using namespace std;
  * Animal class constructor
  *
  * @param string type
+ * @param int index
  */
-Animal::Animal(string type)
+Animal::Animal(string type, int index)
 {
     this->type = type;
+
+    this->index = index;
 }
 
 /**
@@ -25,4 +28,28 @@ Animal::Animal(string type)
 string Animal::getType()
 {
     return this->type;
+}
+
+/**
+ * Returns animal index
+ *
+ * @param void
+ * @return int
+ */
+int Animal::getIndex()
+{
+    return this->index;
+}
+
+/**
+ * Returns whether this animal is older than other
+ * This animal is older if its index is lower,
+ * as that would imply that the animal was inserted sooner
+ *
+ * @param Animal& other
+ * @return bool
+ */
+bool Animal::isOlderThan(Animal& other)
+{
+    return this->index < other.getIndex();
 }
