4f9f04757fb2638babddc187de03e17c5b450441
Mayank Amencherla <mayankamencherla24@gmail.com>
2018-11-07 15:08:32 +0000

    Add files via upload

---
+++ trees-and-graphs/num-paths.cpp
@@ -0,0 +1,68 @@
+#include "binary-search-tree.cpp"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Write an algorithm to check num paths that sum to k in the tree
+ *
+ * @see binary-search-tree.cpp
+ */
+
+/**
+ * Creates a tree of minimal height
+ *
+ * @param BinarySearchTree& bst
+ * @param vectorint>& v
+ * @param int start
+ * @param int end
+ * @return void
+ */
+void minimalTree(BinarySearchTree& bst, vector<int>& v, int start, int end)
+{
+    if (start > end) return;
+
+    int mid = (start + end) / 2;
+
+    bst.insert(v[mid]);
+
+    minimalTree(bst, v, start, mid-1);
+
+    minimalTree(bst, v, mid+1, end);
+}
+
+/**
+ * Creates a tree of minimal height
+ *
+ * @param BinarySearchTree& bst
+ * @param vectorint>& v
+ * @return void
+ */
+void minimalTree(BinarySearchTree& bst, vector<int>& v)
+{
+    minimalTree(bst, v, 0, v.size()-1);
+}
+
+int main()
+{
+    BinarySearchTree bst = BinarySearchTree();
+
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    printf("The height of the tree before insertion %d\n", bst.getHeight());
+
+    cout << endl;
+
+    minimalTree(bst, v);
+
+    cout << endl;
+
+    bst.printLevelOrder();
+
+    printf("The height of the tree after insertion %d\n", bst.getHeight());
+
+    cout << endl;
+
+    printf("Number of paths that sum to 6 %d\n", bst.numPaths(6));
+}
